cd4f5198483522178ef21c1f78900b584ea995b2
George Saker <georgesaker147@gmail.com>
2024-03-11 09:01:38 +0000

    fixed ip adress

--- server/main.py
+++ server/main.py
@@ -7,7 +7,7 @@ class Server:
     def __init__(self):
         print("Starting Server")
         # Set the host and port for the server to listen on
-        localhost = '192.168.40.12287liwequasjb vkehflhefwhjewafjl'
+        localhost = '192.168.40.122'
         port = 64147
 
         # Start a new thread to accept incoming client connections
